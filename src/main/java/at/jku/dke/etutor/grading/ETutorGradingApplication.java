f3aaaefaec850f2a74e29960a05f124cf09ba61f
kevschuetz <kevin.hotz@gmx.at>
2021-04-14 16:06:56 +0200

    springfox

--- src/main/java/at/jku/dke/etutor/grading/ETutorGradingApplication.java
+++ src/main/java/at/jku/dke/etutor/grading/ETutorGradingApplication.java
@@ -1,12 +1,27 @@
 package at.jku.dke.etutor.grading;
 
 
+import at.jku.dke.etutor.grading.rest.ETutorGradingController;
+import at.jku.dke.etutor.grading.rest.ETutorSubmissionController;
+import at.jku.dke.etutor.grading.service.ModuleManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+        ETutorGradingController.class,
+        ETutorSubmissionController.class,
+        ModuleManager.class
+})
 @SpringBootApplication
 public class ETutorGradingApplication {
 
@@ -14,4 +29,12 @@ public class ETutorGradingApplication {
         SpringApplication.run(ETutorGradingApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis( RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
