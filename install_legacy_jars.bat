aaddbdd2c1330a9e0a16b9b1aa5d463e8a417e21
kevschuetz <kevin.hotz@gmx.at>
2022-12-28 15:13:10 +0100

    legacy-jars
    added batch file for installation of legacy-jars.

---
+++ install_legacy_jars.bat
@@ -0,0 +1,10 @@
+call mvn install:install-file -Dfile=./src/main/resources/lib/xml.jar -DgroupId=at.jku.dke.etutor -DartifactId=legacy.javax.xml -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=./src/main/resources/lib/xml-apis.jar -DgroupId=at.jku.dke.etutor -DartifactId=legacy.javax.xml-apis -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=./src/main/resources/lib/xmlParserAPIs.jar -DgroupId=at.jku.dke.etutor -DartifactId=legacy.javax.parsers -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=./src/main/resources/lib/xmlparserv2.jar -DgroupId=at.jku.dke.etutor -DartifactId=legacy.javax.parsersv2 -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=./src/main/resources/xquery/reflect/DDbE.jar -DgroupId=at.jku.dke.etutor -DartifactId=legacy.com.ibm.DDbE -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=./src/main/resources/xquery/reflect/xerces.jar -DgroupId=at.jku.dke.etutor -DartifactId=legacy.javax.xerces -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=./src/main/resources/lib/AbcDatalog-0.5.0.jar -DgroupId=at.jku.dke.etutor -DartifactId=legacy.edu.harvard.seas.pl.abcdatalog -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+:: mvn install:install-file -Dfile=./src/main/java/at/jku/dke/etutor/modules/pm/lib/OpenXES/OpenXES.jar -DgroupId=org.deckfour -DartifactId=OpenXES -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+:: mvn install:install-file -Dfile=./src/main/java/at/jku/dke/etutor/modules/pm/lib/OpenXES/Spex.jar -DgroupId=org.deckfour -DartifactId=Spex -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call pause
---
+++ src/main/java/at/jku/dke/etutor/objects/dispatcher/readme.txt
@@ -0,0 +1,4 @@
+Repository for transformation to objects-project for all DTOs.
+
+1. Refactor all DTOs to this package for all etutorpp-projects
+2. Create objects-project and switch
