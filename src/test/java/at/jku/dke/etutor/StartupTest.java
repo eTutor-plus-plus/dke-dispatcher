c45be4bd96da7ea26ee5d209f5ef8c306e04d1ed
kevschuetz <kevin.hotz@gmx.at>
2023-03-23 09:37:30 +0100

    test activation

--- src/test/java/at/jku/dke/etutor/StartupTest.java
+++ src/test/java/at/jku/dke/etutor/StartupTest.java
@@ -1,10 +1,11 @@
 package at.jku.dke.etutor;
 
+import at.jku.dke.etutor.grading.ETutorGradingApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
-@SpringBootTest
+@SpringBootTest(classes = ETutorGradingApplication.class)
 @Profile("test")
 class StartupTest {
 
