b9a2eafdf7ca00b835420b16d6e7296840cf82c8
Gerald <122382776+k12119624@users.noreply.github.com>
2023-11-27 15:49:17 +0100

    turn NormalFormLevel into enum
    As the class was added before enums were introduced, and its behaviour and usage exactly mimics that of an enum, this seems like a good idea.

--- src/main/java/at/jku/dke/etutor/modules/nf/model/NormalformLevel.java
+++ src/main/java/at/jku/dke/etutor/modules/nf/model/NormalformLevel.java
@@ -2,38 +2,7 @@ package at.jku.dke.etutor.modules.nf.model;
 
 import java.io.Serializable;
 
-public class NormalformLevel implements Serializable {
+public enum NormalformLevel implements Serializable{
 
-	private final String degree;
-	private final int intRepresentation;
-
-	public static final NormalformLevel FIRST = new NormalformLevel("FIRST", 1);
-	public static final NormalformLevel SECOND = new NormalformLevel("SECOND", 2);
-	public static final NormalformLevel THIRD = new NormalformLevel("THIRD", 3);
-	public static final NormalformLevel BOYCE_CODD = new NormalformLevel("BOYCE_CODD", 4);
-
-	protected NormalformLevel(String degree, int intRepresentation) {
-		this.degree = degree;
-		this.intRepresentation = intRepresentation;
-	}
-
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		
-		if (!(obj instanceof NormalformLevel)) {
-			return false;
-		}
-
-		return this.degree.equals(obj.toString());
-	}
-	
-	public int hashCode(){
-		return this.intRepresentation;
-	}
-	
-	public String toString() {
-		return this.degree;
-	}
+	FIRST, SECOND, THIRD, BOYCE_CODD
 }
