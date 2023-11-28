637bf77e199e05b06954e493d52d95b394d397db
knolukas <knogler.lukas@gmail.com>
2023-11-28 19:16:09 +0100

    evaluator fertig

--- src/main/java/at/jku/dke/etutor/modules/drools/report/DroolsReport.java
+++ src/main/java/at/jku/dke/etutor/modules/drools/report/DroolsReport.java
@@ -3,6 +3,7 @@ package at.jku.dke.etutor.modules.drools.report;
 import at.jku.dke.etutor.core.evaluation.DefaultReport;
 import at.jku.dke.etutor.modules.drools.analysis.DroolsAnalysis;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 public class DroolsReport extends DefaultReport implements Serializable {
@@ -12,18 +13,18 @@ public class DroolsReport extends DefaultReport implements Serializable {
         this.droolsAnalysis = droolsAnalysis;
     }
 
-    public void getReport(){
-        if (this.droolsAnalysis.hasSyntaxError() || this.droolsAnalysis.hasSemantikError()){
-            this.setShowErrorDescription(true);
-            if (this.droolsAnalysis.hasSemantikError()){
-                this.setError("Die Lösung hat semantische Fehler!");
-            }
-            if (this.droolsAnalysis.hasSyntaxError()) {
-                this.setError("Die Lösung hat Syntax Fehler!");
-            }
-            if (this.droolsAnalysis.hasSyntaxError() && this.droolsAnalysis.hasSemantikError()) {
-                this.setError("Die Lösung hat Syntax und Semantik Fehler!");
-            }
-        }
-    }
+//    public void getReport() throws ReflectiveOperationException, IOException {
+//        if (this.droolsAnalysis.hasSyntaxError() || this.droolsAnalysis.hasSemantikError()){
+//            this.setShowErrorDescription(true);
+//            if (this.droolsAnalysis.hasSemantikError()){
+//                this.setError("Die Lösung hat semantische Fehler!");
+//            }
+//            if (this.droolsAnalysis.hasSyntaxError()) {
+//                this.setError("Die Lösung hat Syntax Fehler!");
+//            }
+//            if (this.droolsAnalysis.hasSyntaxError() && this.droolsAnalysis.hasSemantikError()) {
+//                this.setError("Die Lösung hat Syntax und Semantik Fehler!");
+//            }
+//        }
+//    }
 }
