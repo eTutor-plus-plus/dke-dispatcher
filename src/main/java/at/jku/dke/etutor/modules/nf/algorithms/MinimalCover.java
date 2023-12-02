f17181ab0d0e4a0ce7c7445481169d2093746157
Gerald <122382776+k12119624@users.noreply.github.com>
2023-12-02 22:13:30 +0100

    IntelliJ and SonarLint optimizations in MinimalCover
    Also, I replaced two calls of dependencies.toArray() with a single one. This should be fine, as the Collection "dependencies" did not change over the course of the loop.

--- src/main/java/at/jku/dke/etutor/modules/nf/algorithms/MinimalCover.java
+++ src/main/java/at/jku/dke/etutor/modules/nf/algorithms/MinimalCover.java
@@ -6,17 +6,21 @@ import java.util.*;
 
 public class MinimalCover {
 	
+	private MinimalCover() {
+		// This class is not meant to be instantiated. (Gerald Wimmer, 2023-12-02)
+	}
+	
 	public static Set<FunctionalDependency> execute(Collection<FunctionalDependency> dependencies){
 		//DERIVE CANONICAL FORM OF FUNCTIONAL DEPENDENCIES
 		Set<FunctionalDependency> minimalCover = unfold(dependencies);
 
 		//DELETING EXTRANEOUS ATTRIBUTES
-		Map<FunctionalDependency, Vector<String>> extraneousAttributes = calculateExtraneousAttributes(minimalCover);
+		Map<FunctionalDependency, List<String>> extraneousAttributes = calculateExtraneousAttributes(minimalCover);
 
-		for (FunctionalDependency keyFD : extraneousAttributes.keySet()){
+		for (Map.Entry<FunctionalDependency, List<String>> curEntry : extraneousAttributes.entrySet()){
 			for (FunctionalDependency currFD : minimalCover){
-				if (currFD.equals(keyFD)){
-					currFD.removeLhsAttributes(extraneousAttributes.get(keyFD));
+				if (currFD.equals(curEntry.getKey())){
+					currFD.removeLhsAttributes(curEntry.getValue());
 				}
 			}
 		}
@@ -31,7 +35,7 @@ public class MinimalCover {
 	}
 
 	public static Set<FunctionalDependency> calculateRedundantFunctionalDependencies(Collection<FunctionalDependency> dependencies){
-		Vector<FunctionalDependency> tempDependencies = new Vector<>();
+		List<FunctionalDependency> tempDependencies = new LinkedList<>();
 		HashSet<FunctionalDependency> redundantDependencies = new HashSet<>();
 
         for (FunctionalDependency currDependency : dependencies) {
@@ -49,10 +53,10 @@ public class MinimalCover {
 	}
 
 
-	public static Map<FunctionalDependency, Vector<String>> calculateExtraneousAttributes (Collection<FunctionalDependency> dependencies) {
-		List<FunctionalDependency> tempFDs = new Vector<>();
+	public static Map<FunctionalDependency, List<String>> calculateExtraneousAttributes (Collection<FunctionalDependency> dependencies) {
+		List<FunctionalDependency> tempFDs = new LinkedList<>();
 		FunctionalDependency tempFD = new FunctionalDependency();
-		Map<FunctionalDependency, Vector<String>> extraneousAttributes = new HashMap<>();
+		Map<FunctionalDependency, List<String>> extraneousAttributes = new HashMap<>();
 
         for (FunctionalDependency currFD : dependencies) {
 			for (String currAttribute : currFD.getLhsAttributes()) {
@@ -83,15 +87,16 @@ public class MinimalCover {
 
 	public static Set<FunctionalDependency> fold(Collection<FunctionalDependency> dependencies) {
 		Set<FunctionalDependency> foldedDependencies = new HashSet<>();
-		List<FunctionalDependency> processedDependencies = new Vector<>();
+		Set<FunctionalDependency> processedDependencies = new HashSet<>();
 
-		for (int i = 0; i < dependencies.size(); i++) {
-			FunctionalDependency currDependency = (FunctionalDependency)dependencies.toArray()[i];
+		FunctionalDependency[] dependencyArray = dependencies.toArray(new FunctionalDependency[0]);
+		for (int i = 0; i < dependencyArray.length; i++) {
+			FunctionalDependency currDependency = dependencyArray[i];
 
 			if (!processedDependencies.contains(currDependency)) {
 
-				for (int j = i; j < dependencies.size(); j++) {
-					FunctionalDependency compDependency = (FunctionalDependency)dependencies.toArray()[j];
+				for (int j = i; j < dependencyArray.length; j++) {
+					FunctionalDependency compDependency = dependencyArray[j];
 
 					if (compareLHS(compDependency, currDependency)) {
 						currDependency.addAllRhsAttributes(compDependency.getRhsAttributes());
