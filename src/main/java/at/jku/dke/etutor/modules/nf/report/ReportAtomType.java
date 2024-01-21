509258af5afb4b0b1e2fb6dc0bfbf905f9fce4ac
Gerald <122382776+k12119624@users.noreply.github.com>
2024-01-21 20:07:16 +0100

    convert ReportAtomType to enum

--- src/main/java/at/jku/dke/etutor/modules/nf/report/ReportAtomType.java
+++ src/main/java/at/jku/dke/etutor/modules/nf/report/ReportAtomType.java
@@ -1,19 +1,15 @@
 package at.jku.dke.etutor.modules.nf.report;
 
-import java.io.Serializable;
+public enum ReportAtomType {
+	INFO("Info"),
+	ERROR("Error"),
+	WARNING("Warning");
 
-public class ReportAtomType implements Serializable {
-
-	private final String name;
-
-	public static final ReportAtomType INFO = new ReportAtomType("Info");
-	public static final ReportAtomType ERROR = new ReportAtomType("Error");
-	public static final ReportAtomType WARNING = new ReportAtomType("Warning");
-
-	protected ReportAtomType(String reportAtomTypeName) {
-		super();
+	ReportAtomType(String reportAtomTypeName) {
 		this.name = reportAtomTypeName;
 	}
+
+	private final String name;
 
 	public String toString(){
 		return this.name;
@@ -21,17 +17,5 @@ public class ReportAtomType implements Serializable {
 
 	public String getName(){
 		return this.name;
-	}
-
-	public boolean equals(Object o){
-		if (o == null){
-			return false;
-		}
-
-		if (o instanceof ReportAtomType){
-			return this.name == ((ReportAtomType)o).getName();
-		} else {
-			return false;
-		}
 	}
 }
